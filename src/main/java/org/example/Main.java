f38aed83a2c7798b965b3f44461698310dc3a361
Chmihi <chmihidouae0@gmail.com>
2024-05-23 16:19:11 +0100

    modifie again main.java par main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bonjour et bon courage dans votre examen en DevOps");
-        System.out.println("commit du deuxieme branche");
+        System.out.println("commit du deuxieme branche chab=nger par main");
 
         System.out.println("Main change");
 
